3c43dba63be2938b796382161e0a8efaa8eba59e
news4hana103 <sap-tech-share@gmail.com>
2023-09-03 10:34:53 -0700

    Controller modified

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -9,20 +9,34 @@ sap.ui.define([
 
         return Controller.extend("testattachment.controller.View1", {
             onInit: function () {
-                var that=this;
-                oAttachmentComponentPromise = this.getOwnerComponent().createComponent({
-                        usage:"attachmentReuseComponent",
+                // var that=this;
+                // oAttachmentComponentPromise = this.getOwnerComponent().createComponent({
+                //         usage:"attachmentReuseComponent",
 
-                        settings: {
-                            mode: "D", //owner._mode,
-                            objectKey: "1", //owner._objectKey,
-                            objectType: "ZOBJTEST1", //owner._objectType,
-                            semanticObject: "Attach", //owner._semanticObject,
-                            onupload: [that.handleUpload, that]}
-                    });
-                oAttachmentComponentPromise.then(function(successValue){
-                that.byId("attachmentComponentContainer").setComponent(successValue);
-                });
+                //         settings: {
+                //             mode: "D", //owner._mode,
+                //             objectKey: "1", //owner._objectKey,
+                //             objectType: "ZOBJTEST1", //owner._objectType,
+                //             semanticObject: "Attach", //owner._semanticObject,
+                //             onupload: [that.handleUpload, that]}
+                //     });
+                // oAttachmentComponentPromise.then(function(successValue){
+                // that.byId("attachmentComponentContainer").setComponent(successValue);
+                // });
+                var attachmentView = this.byId("attachmentComponentContainer");
+                var that = this;
+                var oComp = sap.ui.getCore().createComponent({
+                                        name: "sap.se.mi.plm.lib.attachmentservice.attachment",
+                                        id: "attachmentsrv.TestComponent1",
+                                        settings: {
+                                            mode: "D", //owner._mode,
+                                            objectKey: "1", //owner._objectKey,
+                                            objectType: "ZOBJTEST1", //owner._objectType,
+                                            semanticObject: "attachment", //owner._semanticObject,
+                                            onupload: [that.handleUpload, that]
+                                        }
+                                    });
+                attachmentView.setComponent(oComp);
             }
         });
     });
